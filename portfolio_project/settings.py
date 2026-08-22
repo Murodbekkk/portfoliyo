3807a5573bdb684b523e9e07a50bd67816d65bcd
max271999 <full.stic.dasturchi@gmail.com>
2026-08-22 15:25:24 +0500

    1

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -118,7 +118,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'main' / 'static']
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'main' / 'static',
+]
+
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
